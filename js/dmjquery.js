00d2d87f77c6711fb3183746878c37437b36530c
morenod <dmorenocuellar@gmail.com>
2016-05-27 00:39:32 -0700

    goodies

---
+++ js/dmjquery.js
@@ -0,0 +1,18 @@
+$("#dev").click(function() {
+ $('body').removeClass();
+ $('body').addClass('class1');
+});
+
+$("#des").click(function() {
+ $('body').removeClass();
+ $('body').addClass('class2');
+});
+
+$("#stu").click(function() {
+ $('body').removeClass();
+ $('body').addClass('class3');
+
+$("#thnk").click(function() {
+ $('body').removeClass();
+ $('body').addClass('class4');
+});
